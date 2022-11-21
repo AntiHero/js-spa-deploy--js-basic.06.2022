53e4267b9c43a96a93adccae1965c4cefbdcfada
Aleksandr Schemelev <prophreaker@gmail.com>
2022-11-21 20:21:39 +0300

    add history api

--- src/index.ts
+++ src/index.ts
@@ -11,7 +11,13 @@ function render(hash?: string) {
 document.body.addEventListener("click", (ev) => {
   if ((ev.target as HTMLElement).matches("a")) {
     const hash = (ev.target as HTMLAnchorElement).hash;
+    
+    ev.preventDefault();
 
+    if (!window.history) throw new Error("History API is not available");
+    
+    history.pushState({}, '', (ev.target as HTMLAnchorElement).href);
+    
     render(hash);
   }
 });
--- webpack.config.ts
+++ webpack.config.ts
@@ -16,6 +16,7 @@ const config: webpack.Configuration = {
     environment: {
       arrowFunction: false,
     },
+    publicPath: '/',
   },
   mode: NODE_ENV,
   resolve: {
@@ -43,6 +44,7 @@ const config: webpack.Configuration = {
     compress: true,
     port: 9000,
     watchFiles: ["public/index.html"],
+    historyApiFallback: true,
   },
 };
 
